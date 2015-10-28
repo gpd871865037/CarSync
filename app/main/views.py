813e35874310482603a840c752a5c178f92b0ec6
GPD <871865037@qq.com>
2015-10-28 22:33:18 +0800

    fix

--- app/main/views.py
+++ app/main/views.py
@@ -18,7 +18,7 @@ def bind_account():
     data = requests.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx54073d86056904da&secret=e102c09b6828c759084407bebc785b08&code="+ code +"&grant_type=authorization_code")
     result = json.loads(data.text)
     openid = result['openid'].encode('utf-8')
-    print str(openid)
+    print type(openid)
     return 'fuck you dong dong one by one~'
     #return render_template('bind_account.html', code=openid)
 
