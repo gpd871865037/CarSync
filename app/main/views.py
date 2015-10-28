e0a452c579f910ed95246489dcde49eb3bc65e05
GPD <871865037@qq.com>
2015-10-28 21:13:04 +0800

    fix

--- app/main/views.py
+++ app/main/views.py
@@ -19,7 +19,7 @@ def bind_account():
     result = json.loads(data.text)
     # openid = result['openid']
     # return render_template('bind_account.html',code=openid)
-    return make_response(data.text)
+    return result
 
 @main.route('/get_info', methods=['POST'])
 def get_info():
