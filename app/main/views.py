5e5376ce8353ec23f62a60a41c6889caeaac968e
GPD <871865037@qq.com>
2015-10-28 21:20:27 +0800

    fix

--- app/main/views.py
+++ app/main/views.py
@@ -17,9 +17,9 @@ def bind_account():
     code = request.args.get('code')
     data = requests.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx54073d86056904da&secret=e102c09b6828c759084407bebc785b08&code="+ code +"&grant_type=authorization_code")
     result = json.loads(data.text)
-    # openid = result['openid']
+    openid = result['openid']
     # return render_template('bind_account.html',code=openid)
-    return type(result['openid'])
+    return openid
 
 @main.route('/get_info', methods=['POST'])
 def get_info():
