ec2dd3248451cda05c2fde2a88c1d244baee8a1e
GPD <871865037@qq.com>
2015-10-28 22:19:04 +0800

    fix

--- app/main/views.py
+++ app/main/views.py
@@ -20,7 +20,7 @@ def bind_account():
     # openid = result['openid']
     # return render_template('bind_account.html', code=openid)
     #return type(str(openid.encode('utf-8')))
-    print type(result['openid'])
+    print type(result['openid'].encode('utf-8'))
     return result['openid']
 
 @main.route('/get_info', methods=['POST'])
